e26188369950d821277fd01374991d8679a54b7e
anirbanbha <anirbanbhatt@gmail.com>
2024-11-28 13:14:53 +0530

    Initial commit

---
+++ src/test/java/com/testingacademy_training/samplecheck/apitest001.java
@@ -0,0 +1,11 @@
+package com.testingacademy_training.samplecheck;
+
+import io.restassured.RestAssured;
+
+public class apitest001 {
+    public static void main(String[] args) {
+        RestAssured.given().baseUri()
+
+
+    }
+}
